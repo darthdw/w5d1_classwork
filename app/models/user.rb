a42a03f172126436db3b15518adc8e6efa6f2c30
darthdw <thebackupname@gmail.com>
2017-08-28 15:42:06 -0700

    New models and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
-  
+
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :session_token, presence: true
 
   after_initialize :ensure_session_token
+
+  has_many :cheers
+  has_many :goals
 
   attr_reader :password
 
